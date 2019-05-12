ec239d94ce25d7563098bc48fec9a990de92d233
Waseem Ahmad <waseem_ch19@hotmail.com>
2019-05-12 17:40:36 -0300

    i Complete maven project "Hello World"

---
+++ javacoding/src/main/java/com/ictexpertsgroup/practice/App.java
@@ -0,0 +1,14 @@
+package com.ictexpertsgroup.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{ 
+
+	public static void main( String[] args )
+    {
+//        System.out.println( "Hello World!" );
+		Util.printMessage("Hello World");
+    }
+}
---
+++ javacoding/src/main/java/com/ictexpertsgroup/practice/Util.java
@@ -0,0 +1,7 @@
+package com.ictexpertsgroup.practice;
+
+public class Util {
+	public static void printMessage(String str) {
+		System.out.println(str);
+	}
+}
